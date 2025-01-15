b8e108fa2eeba10408af7d741fe025a1bf1f7a81
ayule <68344743+ayule@users.noreply.github.com>
2025-01-14 21:58:14 -0700

    define Odds struct

---
+++ internal/data/odds.go
@@ -0,0 +1,32 @@
+package data
+
+import (
+	"time"
+)
+
+type Outcome struct {
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+}
+
+type Market struct {
+	Key        string    `json:"key"`
+	LastUpdate time.Time `json:"last_update"`
+	Outcomes   []Outcome `json:"outcomes"`
+}
+
+type Bookmaker struct {
+	Key        string    `json:"key"`
+	Title      string    `json:"title"`
+	LastUpdate time.Time `json:"last_update"`
+	Markets    []Market  `json:"markets"`
+}
+
+type Odds struct {
+	ID         string      `json:"id"`
+	SportKey   string      `json:"sport_key"`
+	SportTitle string      `json:"sport_title"`
+	HomeTeam   string      `json:"home_team"`
+	AwayTeam   string      `json:"away_team"`
+	Bookmakers []Bookmaker `json:"bookmakers"`
+}
