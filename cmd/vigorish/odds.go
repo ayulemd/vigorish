4eaf41603c4397520aed107601d9123e3db79896
ayule <68344743+ayule@users.noreply.github.com>
2025-01-15 21:56:48 -0700

    request and decode odds data

---
+++ cmd/vigorish/odds.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+
+	"github.com/ayulemd/vigorish/internal/data"
+)
+
+var odds []data.Odds
+
+func (app *application) getWithQuery(baseURL string, params map[string]string) error {
+	parsedURL, err := url.Parse(baseURL)
+	if err != nil {
+		return err
+	}
+
+	query := parsedURL.Query()
+	for key, value := range params {
+		query.Set(key, value)
+	}
+	parsedURL.RawQuery = query.Encode()
+
+	req, err := http.NewRequest(http.MethodGet, parsedURL.String(), nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Accept", "application/json")
+
+	res, err := app.client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", res.StatusCode)
+	}
+
+	app.readJSON(res, &odds)
+
+	pretty, err := json.MarshalIndent(odds, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(pretty))
+
+	return nil
+}
