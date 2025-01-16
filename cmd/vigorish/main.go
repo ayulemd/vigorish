0c9303d2b29eb16516a79f141a93c00a4e4dbc3a
ayule <68344743+ayule@users.noreply.github.com>
2025-01-15 21:57:44 -0700

    provide parameters for app's API request

--- cmd/vigorish/main.go
+++ cmd/vigorish/main.go
@@ -2,8 +2,47 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
+type config struct {
+	apiKey string
+}
+
+type application struct {
+	config config
+	client *http.Client
+}
+
 func main() {
-	fmt.Println("hello world!")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	var cfg config
+
+	cfg.apiKey = os.Getenv("THE_ODDS_API_KEY")
+
+	app := &application{
+		config: cfg,
+		client: &http.Client{},
+	}
+
+	fmt.Println("Calling API")
+
+	baseURL := "https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds"
+
+	params := map[string]string{
+		"apiKey":     app.config.apiKey,
+		"regions":    "us",
+		"markets":    "h2h",
+		"oddsFormat": "american",
+	}
+
+	app.getWithQuery(baseURL, params)
 }
