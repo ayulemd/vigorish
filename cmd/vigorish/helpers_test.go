2cb14479e2fb9eb30df6fde8178cfbd2b093116b
ayulemd <140634306+ayulemd@users.noreply.github.com>
2025-01-28 20:47:40 -0700

    unit tests for calculateVigorish helper method

--- cmd/vigorish/helpers_test.go
+++ cmd/vigorish/helpers_test.go
@@ -12,12 +12,12 @@ import (
 var app *application
 
 func TestMain(m *testing.M) {
+	fmt.Println("Setting up application instance before tests...")
+
 	app = &application{
 		config: config{},
 		client: &http.Client{},
 	}
-
-	fmt.Println("Setting up application instance before tests...")
 
 	exitCode := m.Run()
 
@@ -26,7 +26,7 @@ func TestMain(m *testing.M) {
 	os.Exit(exitCode)
 }
 
-func TestImpliedProbability(t *testing.T) {
+func TestCalculateImpliedProbability(t *testing.T) {
 	testCases := []struct {
 		name     string
 		price    int64
@@ -40,7 +40,7 @@ func TestImpliedProbability(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			result := app.impliedProbability(tc.price)
+			result := app.calculateImpliedProbability(tc.price)
 
 			expectedDecimal, err := decimal.NewFromString(tc.expected)
 			if err != nil {
@@ -49,6 +49,46 @@ func TestImpliedProbability(t *testing.T) {
 
 			if !result.Equal(expectedDecimal) {
 				t.Errorf("impliedProbability(%d) = %s; expected %s", tc.price, result.String(), expectedDecimal.String())
+			}
+		})
+	}
+}
+
+func TestCalculateVigorish(t *testing.T) {
+	testCases := []struct {
+		name                 string
+		impliedProbabilities []decimal.Decimal
+		expected             string
+	}{
+		{
+			"standard -110 moneyline",
+			[]decimal.Decimal{
+				decimal.RequireFromString("52.38"),
+				decimal.RequireFromString("52.38"),
+			},
+			"4.76",
+		},
+		{
+			"-124 vs. 106 moneyline",
+			[]decimal.Decimal{
+				decimal.RequireFromString("55.36"),
+				decimal.RequireFromString("48.54"),
+			},
+			"3.90",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := app.calculateVigorish(tc.impliedProbabilities)
+
+			expectedDecimal, err := decimal.NewFromString(tc.expected)
+			if err != nil {
+				t.Fatalf("Invalid expected decimal string %q: %v", tc.expected, err)
+			}
+
+			if !result.Equal(expectedDecimal) {
+				t.Errorf("Implied probabilities: %v,  vigorish = %s; expected %s", tc.impliedProbabilities, result.String(), expectedDecimal.String())
 			}
 		})
 	}
